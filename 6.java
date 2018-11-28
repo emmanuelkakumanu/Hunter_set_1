97ec740c15245586f593e1a1c79ccd2e15ae4203
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-11-28 21:30:23 +0530

    print he no which is repeated first

---
+++ 6.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		int st=0;
+		for(int i=0;i<n;i++)
+		{
+			int cnt=0;
+			for(int j=0;j<n;j++)
+			{
+				if(a[i]==a[j])
+				{
+					cnt++;
+				}
+			}
+			if(cnt==2)
+			{
+				System.out.print(a[i]);
+				st=1;
+				break;
+			}
+		}
+		if(st==0)
+		System.out.print("unique");
+	}
+}
