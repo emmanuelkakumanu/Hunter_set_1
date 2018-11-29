06ee8f596430739cfea8cc0c9380734828e32825
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-11-29 19:00:57 +0530

    sum closest to zero

---
+++ 9.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=s.nextInt();
+		boolean st=false;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(a[i]+a[j]==0)
+				{
+					System.out.print(a[i]+" "+a[j]);st=true;break;
+				}
+			}
+			if(st)
+			break;
+		}
+	}
+}
